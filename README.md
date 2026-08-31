f3889fd2786e15dcfac5f80d08352275faa5280d
Cuongdz <70798830+haicuong@users.noreply.github.com>
2026-08-31 10:52:21 +0700

    TailwindCSS setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# personal-website
+# Personal website
+
 Personal website contain About me, Porfolio, Blogs
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+// vite.config.ts
+// import { resolve } from "path";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  /* build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        blog: resolve(__dirname, "blog/index.html"),
+        projects: resolve(__dirname, "projects/index.html"),
+      },
+    },
+  }, */
+  plugins: [tailwindcss()],
+});
